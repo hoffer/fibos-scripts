ddb1fee8b5e9308a0ee1e1de2ce7dc7053b58fbd
Hoffer <hoffer1127@gmail.com>
2018-09-16 00:42:48 -0400

    update producer info
    Signed-off-by: Hoffer <hoffer1127@gmail.com>

--- setproducerjson-bitzebitze11.js
+++ setproducerjson-bitzebitze11.js
@@ -12,28 +12,39 @@ var client = new FibosClient(config);
 
 jsonData = {
     "producer_account_name": "bitzebitze11",
-     "producer_public_key": "FO56K4J8eESbLRs4YhmHNF1QsWDySUrvWWJvYWaiTNZyXT8J4aH7",
-     "org": {
-       "candidate_name": "Bitze",
-       "website": "http://bitze.site",
-       "email":"hello@bitze.site",
-       "location": {
-         "name": "Oregon",
-         "country": "US",
-         "latitude": 45.5234,
-         "longitude": -122.6762
-       }
-     },
-     "nodes": [
-       {
-         "location": {
+    "producer_public_key": "FO56K4J8eESbLRs4YhmHNF1QsWDySUrvWWJvYWaiTNZyXT8J4aH7",
+    "org": {
+        "candidate_name": "Bitze",
+        "website": "http://bitze.site",
+        "email":"support@bitze.site",
+        "location": {
             "name": "Oregon",
             "country": "US",
             "latitude": 45.5234,
             "longitude": -122.6762
+        }
+    },
+    "nodes": [
+        {
+           "location": {
+             "name": "California",
+             "country": "US",
+             "latitude": 37.338,
+             "longitude": -121.8
+           },
+           "node_type" : "producer"
          },
-         "node_type" : "producer"
-       },
+        {
+         "location": {
+             "name": "Oregon",
+             "country": "US",
+             "latitude": 45.5234,
+             "longitude": -122.6762
+          },
+         "node_type" : "full",
+         "api_endpoint": "http://api.bitze.site",
+         "ssl_endpoint": "https://api.bitze.site"
+        },
        {
          "location": {
              "name": "Oregon",
